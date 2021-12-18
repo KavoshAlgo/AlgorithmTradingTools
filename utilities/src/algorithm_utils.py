b1c83f0945265a70431e974cbb04ed9d38e05aa2
mehrdad <sayad.mehrdad@gmail.com>
2021-12-18 17:46:25 +0330

    add validate orderbook into the tools repo

---
+++ utilities/src/algorithm_utils.py
@@ -0,0 +1,7 @@
+def validate_orderbooks(orderbooks: list):
+    for orderbook in orderbooks:
+        if orderbook is None or 'bids' not in orderbook or 'asks' not in orderbook:
+            return False
+        if len(orderbook['bids']) <= 0 or len(orderbook['asks']) <= 0:
+            return False
+    return True

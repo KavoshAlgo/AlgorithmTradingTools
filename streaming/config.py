47dc1618446aae5a7b38cc3b0ceaf32c7ffc1d4c
mehrdad <sayad.mehrdad@gmail.com>
2022-01-05 21:47:27 +0330

    add stream_consumer

---
+++ streaming/config.py
@@ -0,0 +1,10 @@
+
+
+class StreamConfig:
+    KAFKA_SERVER = ""
+    # consumer
+    CONSUMER_LOGGER = False
+    CONSUMER_LOGGER_PATH = ""
+    # producer
+    PRODUCER_LOGGER = False
+    PRODUCER_LOGGER_PATH = ""
---
+++ streaming/src/stream_consumer.py
@@ -0,0 +1,30 @@
+import json
+
+from monitoring.src.logger import Logger
+from streaming.config import StreamConfig
+from kafka import KafkaConsumer
+
+
+class StreamConsumer:
+    def __init__(self, topic, **args):
+        """
+        create a connection to the kafka stream channel and consume the channel
+        :param client_id:  the client id of consumer
+        :param topic:  the topic of stream channel in Kafka
+        :param timeout:  the timeout of stream connection for receiving data
+        :param auto_offset_reset:  "earliest" or "latest", start from last offset we consume or the latest one, Default:
+         "earliest"
+        :return an object with a consumer attribute
+        """
+        self.logger = Logger(
+            main=StreamConfig.CONSUMER_LOGGER,
+            file_name=StreamConfig.CONSUMER_LOGGER_PATH
+        )
+        args["value_deserializer"] = lambda m: json.loads(m.decode('ascii'))
+        self.logger.info("create a connection to kafka channel(%s) as a consumer ...." % topic)
+        self.consumer = KafkaConsumer(
+            topics=topic,
+            bootstrap_servers=[StreamConfig.KAFKA_SERVER],
+            **args
+        )
+        self.logger.info("%s consumer connection is established" % topic)

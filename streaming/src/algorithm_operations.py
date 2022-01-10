f7eda4aba09d4ccf90610e0656ac907688a1a332
mehrdad <sayad.mehrdad@gmail.com>
2022-01-10 21:17:32 +0330

    add algorithm operation

---
+++ streaming/src/algorithm_operations.py
@@ -0,0 +1,63 @@
+import threading
+import random
+import string
+import time
+
+from streaming.src.stream_consumer import StreamConsumer
+from streaming.src.stream_producer import StreamProducer
+from streaming.config import StreamConfig
+
+from monitoring.src.logger import Logger
+
+
+class AlgorithmOperations:
+    def __init__(self, operation_producer: StreamProducer, user_data_consumer: StreamConsumer, producer_topic: str):
+        """
+        create an object of the AlgorithmOperations that handle communication with broker side
+        :rtype: AlgorithmOperations object
+        :param operation_producer: the producer instance
+        :param user_data_consumer: the consumer instance
+        :param producer_topic: the consumer stream channel
+        """
+        self.operation_producer = operation_producer
+        self.user_data_consumer = user_data_consumer
+
+        self.producer_topic = producer_topic
+        '''
+            handle jobs with this dictionary
+        '''
+        self.jobs = {}
+        self.logger = Logger(StreamConfig.ALGORITHM_OPERATION_LOGGER, StreamConfig.ALGORITHM_OPERATION_LOGGER_PATH)
+
+    def start(self):
+        try:
+            threading.Thread(name="start_consume_user_data", target=self.consume, daemon=False).start()
+        except Exception as ex:
+            self.logger.error("Could not start the consume_user_data thread :" + str(ex))
+
+    def consume(self):
+        for item in self.user_data_consumer.consumer:
+            if "job" in item:
+                self.jobs[item["job_id"]] = item
+
+    def send_order(self, **kwargs):
+        return self.do_job("send_order", **kwargs)
+
+    def cancel_order(self, **kwargs):
+        return self.do_job("cancel_order", **kwargs)
+
+    def do_job(self, job, **kwargs):
+        job_id = self.generate_id()
+        self.operation_producer.send(self.producer_topic, {
+            "job_id": job_id,
+            "job": job,
+            "job_args": kwargs
+        })
+        while True:
+            if self.jobs[job_id]:
+                return self.jobs[job_id]["response"], self.jobs[job_id]["status"]
+
+    def generate_id(self):
+        job_id = ''.join(random.choices(string.ascii_lowercase, k=5))
+        self.jobs[job_id] = None
+        return job_id

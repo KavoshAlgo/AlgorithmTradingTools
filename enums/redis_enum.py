fc28a141c839898365b9a2f7b04dedc3488a6346
mehrdad <sayad.mehrdad@gmail.com>
2022-01-30 21:58:27 +0330

    add trap cancel signal

--- enums/redis_enum.py
+++ enums/redis_enum.py
@@ -16,3 +16,6 @@ class RedisEnums:
         MARKET = "_market_data"  # standard: BROKER_NAME + Stream.MARKET
         USER_DATA = "_broker_user_data_"  # standard: BROKER_NAME + Stream.USER_DATA + USERNAME
         USER_REQUEST = "_broker_user_request_"  # standard: BROKER_NAME + Stream.USER_REQUEST + USERNAME
+
+    class Signals:
+        TRAP_CANCEL_SIGNAL = "_trap_cancel_signal_"  # standard: BROKER_NAME + Signals.TRAP_CANCEL_SIGNAL + USERNAME

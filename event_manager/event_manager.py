553c9797f7821131b28af591cfb997831d655817
mehrdad <sayad.mehrdad@gmail.com>
2022-02-26 17:32:32 +0330

    add event topic into the examiner

--- event_manager/event_manager.py
+++ event_manager/event_manager.py
@@ -17,11 +17,20 @@ class EventManager:
     def start(self):
         self.event_examiner.start()
 
-    def get_new_event(self):
-        return Event(self.generate_event_id())
+    def get_new_event(self, topic : str) -> Event:
+        event = Event(event_id=self.generate_event_id(),  event_topic=topic)
+        self.add_topic_event_into_examiner(event)
+        return event
 
     @staticmethod
-    def generate_event_id():
+    def generate_event_id() -> str:
         hash_string_id = ''.join(random.choices(string.ascii_lowercase, k=EventManagerConfig.ID_HASH_STRING_LENGTH))
         timestamp = str(int(time.time() * (10 ** 6)))
         return timestamp + "_" + hash_string_id
+
+    def add_topic_event_into_examiner(self, event: Event):
+        if event.EVENT_TOPIC in self.event_examiner.topics_events:
+            self.event_examiner.topics_events[event.EVENT_TOPIC].append(event.EVENT_ID)
+        else:
+            self.event_examiner.topics_events[event.EVENT_TOPIC] = [event.EVENT_ID]
+

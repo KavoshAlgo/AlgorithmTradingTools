98f706b6bf9992e3c31f20d113c37fa4fd416295
GeTLorD <GeT.xeO@Gmail.com>
2022-03-02 13:30:44 +0330

    create class PortfolioBalancing and basic methods.

---
+++ algorithm_tools/portfolio_balancing/portfolio_balancing.py
@@ -0,0 +1,15 @@
+class PortfolioBalancing:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def main(self):
+        pass
+
+    def check_coudition(self):
+        pass
+
+    def ex_change_usdtirt(self):
+        pass

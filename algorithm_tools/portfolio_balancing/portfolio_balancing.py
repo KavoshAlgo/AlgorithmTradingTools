596a3dd0d63c2c07d03f66c9542105c80a177704
GeTLorD <GeT.xeO@Gmail.com>
2022-03-02 17:59:11 +0330

    develop __init__ and start method(create thread on main method).

--- algorithm_tools/portfolio_balancing/portfolio_balancing.py
+++ algorithm_tools/portfolio_balancing/portfolio_balancing.py
@@ -1,22 +1,63 @@
-# TODO fix naming (flow chart)
-# TODO fix naming (in old program) and import new program
-# TODO Write def init
-# TODO Write def start
+import threading
+import time
+
+from monitoring.src.logger import Logger
+
+from storage.redis.src.redis import Redis
+
+from enums.redis_enum import RedisEnums
+from enums.orderbooks import Orderbooks
+from enums.portfolio import Portfolio
+from enums.order import Order
+
+from events.event import Event
+
+
+# TODO fix naming and basic (flow chart)
 # TODO edit def main to new version
 # TODO Write def check_coudition
-# TODO Write ex_change_usdtirt
+# TODO Write def ex_change_usdtirt
+# TODO Write def get_usdt_irt_portfolio
+
 class PortfolioBalancing:
-    def __init__(self):
-        super().__init__()
+
+    def __init__(self, username, broker, trade_value_threshold=0, portfolio_balance_threshold=0,
+                 threshold_factor=0, vol_factor=0, extra_price=0, execution_wait_time=120):
+        self.redis = Redis()
+        self.logger = Logger(True, '')
+        self.trade_value_threshold = trade_value_threshold
+        self.portfolio_balance_threshold = portfolio_balance_threshold
+        self.threshold_factor = threshold_factor
+        self.vol_factor = vol_factor
+        self.extra_price = extra_price
+        self.execution_wait_time = execution_wait_time
+        self.broker = broker
+        self.username = username
+        self.market = 'USDTIRT'
+        self.USDT = 'USDT'
+        self.IRT = 'IRT'
 
     def start(self):
+        try:
+            threading.Thread(name="main", target=self.main, daemon=False).start()
+        except Exception as ex:
+            self.logger.error("Could not start the manage requests :" + str(ex))
+
+    def main(self, broker):
         pass
 
-    def main(self):
+    def check_coudition(self, condition, usdt_portfolio, irt_portfolio):
         pass
 
-    def check_coudition(self):
+    def send_order(self):
         pass
 
     def ex_change_usdtirt(self):
         pass
+
+    def get_usdt_irt_portfolio(self, broker, username):
+        pass
+
+
+if __name__ == '__main__':
+    pass

6945fa507de68cf2bf02770572a6837015eff0b4
GeTLorD <GeT.xeO@Gmail.com>
2022-03-02 14:44:10 +0330

    add TODOs.

--- algorithm_tools/portfolio_balancing/portfolio_balancing.py
+++ algorithm_tools/portfolio_balancing/portfolio_balancing.py
@@ -1,6 +1,13 @@
+# TODO fix naming (flow chart)
+# TODO fix naming (in old program) and import new program
+# TODO Write def init
+# TODO Write def start
+# TODO edit def main to new version
+# TODO Write def check_coudition
+# TODO Write ex_change_usdtirt
 class PortfolioBalancing:
     def __init__(self):
-        pass
+        super().__init__()
 
     def start(self):
         pass

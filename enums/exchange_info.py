f75b6034f2e73d866c30b0540716f40021c82c55
GeTLorD <GeT.xeO@Gmail.com>
2022-03-15 13:26:17 +0330

    add     MIN_VOL = 'min_vol'     MIN_VALUE = 'min_value'     in ExchangeInfo.

--- enums/exchange_info.py
+++ enums/exchange_info.py
@@ -4,6 +4,8 @@ class ExchangeInfo:
     MODE = "mode"
     COINS = 'coins'
     MARKETS = "markets"
+    MIN_VOL = 'min_vol'
+    MIN_VALUE = 'min_value'
 
     class TruncateMode:
         STEP = "step"

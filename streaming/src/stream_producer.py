43042205e087bc4228c53da36ee3e343c0f87153
mehrdad <sayad.mehrdad@gmail.com>
2022-05-31 21:51:17 +0430

    back to previous version

--- streaming/src/stream_producer.py
+++ streaming/src/stream_producer.py
@@ -1,6 +1,5 @@
 import json
 import redis
-import aioredis
 
 from monitoring.src.logger import Logger
 from streaming.config import StreamConfig
@@ -16,20 +15,15 @@ class StreamProducer:
             main=StreamConfig.PRODUCER_LOGGER,
             file_name=StreamConfig.PRODUCER_LOGGER_PATH
         )
-        # self.redis_connection = redis.Redis(
-        #     host=StreamConfig.REDIS_SERVER,
-        #     port=StreamConfig.REDIS_PORT,
-        #     password=StreamConfig.REDIS_PASSWORD)
-        self.redis_connection = aioredis.from_url(
-            StreamConfig.REDIS_SERVER,
+        self.redis_connection = redis.Redis(
+            host=StreamConfig.PRODUCER_REDIS_SERVER,
             port=StreamConfig.REDIS_PORT,
-            password=StreamConfig.REDIS_PASSWORD
-        )
+            password=StreamConfig.REDIS_PASSWORD)
         self.logger.info("create a connection to Redis Server as a producer ....")
         self.logger.info("producer connection is established")
 
-    async def send(self, topic, message, maxlen=None):
-        await self.redis_connection.xadd(
+    def send(self, topic, message, maxlen=None):
+        self.redis_connection.xadd(
             topic,
             {"data": json.dumps(message)},
             maxlen=maxlen

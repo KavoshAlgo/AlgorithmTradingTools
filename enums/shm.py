5b2afc0752292ff282e9af3d7358ca0d60e34d57
elmiado <sayad.mahdi75@gmail.com>
2022-08-22 22:25:07 +0430

    minor change in SHMEnums class.

--- enums/shm.py
+++ enums/shm.py
@@ -2,5 +2,5 @@ class SHMEnums:
     # standard: BROKER_NAME + SHMEnums.PORTFOLIO + USERNAME
     PORTFOLIO = "_portfolio_"
 
-    # standard: BROKER_NAME + SHMEnums.PORTFOLIO + USERNAME
-    ORDERBOOK = "_orderbook_"
+    # standard: BROKER_NAME + SHMEnums.ORDERBOOK + USERNAME
+    ORDERBOOK = "_orderbook"

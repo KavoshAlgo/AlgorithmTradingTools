7253fde23479b5bd0964db720cbeddd3d56cbe2a
mehrdad <sayad.mehrdad@gmail.com>
2022-09-23 18:02:50 +0330

    -add operation request enum

--- enums/operation_request.py
+++ enums/operation_request.py
@@ -1,7 +1,8 @@
-class AlgorithmRequest:
+class OperationRequest:
     JOB_ID = "job_id"
     JOB = "job"
     JOB_ARGS = "job_args"
+    TIME = "time"
     RESPONSE = "response"
     STATUS = "status"
     EVENT_TYPE = "event_type"

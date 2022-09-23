5bf74ddc992b31d8341a31c4a9f10061410a1b98
mehrdad <sayad.mehrdad@gmail.com>
2022-09-23 16:32:44 +0330

    -define new parameters for account info redis set

--- enums/account_info.py
+++ enums/account_info.py
@@ -4,6 +4,9 @@ class AccountInfo:
     PORTFOLIO_MAPPING = "portfolio_mapping"  # mapping for portfolio matrix indices
     PORTFOLIO_MATRIX_SHAPE = "portfolio_matrix_shape"  # shape of portfolio matrix
     PORTFOLIO_SHM_NAME = "portfolio_shm_name"  # shape of portfolio matrix
+    ORDERS_MATRIX_SHAPE = "orders_matrix_shape"  # shape of portfolio matrix
+    ORDERS_SHM_NAME = "orders_shm_name"  # shape of portfolio matrix
+    API_TOKEN = ""  # the api token of this account
 
     class CommissionMode:
         MAKER = 'maker'  # COMMISSION FEE OF MAKER TRADES

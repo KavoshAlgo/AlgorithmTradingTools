b5be02f0c8778b93131b4bff297b85d8c3b1765c
mehrdad <sayad.mehrdad@gmail.com>
2022-12-17 12:53:02 +0330

    - change bid and ask net into the cr(conversion rate)

--- enums/orderbooks.py
+++ enums/orderbooks.py
@@ -24,12 +24,12 @@ class OrderbookMatrix:
 
     ASK_PRICE = "ask_price"
     ASK_VOLUME = "ask_volume"
-    ASK_NET = "ask_net"
+    ASK_CR = "ask_cr"
     ASK_USD_VALUE = "ask_usd_value"
 
     BID_PRICE = "bid_price"
     BID_VOLUME = "bid_volume"
-    BID_NET = "bid_net"
+    BID_CR = "bid_cr"
     BID_USD_VALUE = "bid_usd_value"
 
 
@@ -37,10 +37,10 @@ ORDERBOOK_DTYPE = np.dtype([
     (OrderbookMatrix.INDEX, np.int32),
     (OrderbookMatrix.ASK_PRICE, np.float32),
     (OrderbookMatrix.ASK_VOLUME, np.float32),
-    (OrderbookMatrix.ASK_NET, np.float32),
+    (OrderbookMatrix.ASK_CR, np.float32),
     (OrderbookMatrix.ASK_USD_VALUE, np.float32),
     (OrderbookMatrix.BID_PRICE, np.float32),
     (OrderbookMatrix.BID_VOLUME, np.float32),
-    (OrderbookMatrix.BID_NET, np.float32),
+    (OrderbookMatrix.BID_CR, np.float32),
     (OrderbookMatrix.BID_USD_VALUE, np.float32),
 ])
